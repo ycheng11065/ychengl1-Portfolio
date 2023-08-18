251283e78264f352b288b582e096025d2aa5ce1d
Yu Cheng Li <ychengli11065@gmail.com>
2023-08-18 02:13:33 -0700

    first commit, setting everything up using typescript and mantine

---
+++ src/components/mainPage.tsx
@@ -0,0 +1,11 @@
+import { Text } from '@mantine/core';
+
+const MainPage = () => {
+  return (
+    <>
+      <Text>Welcome to the main page, this is gonna be a fire portfolio website</Text>
+    </>
+  );
+};
+
+export default MainPage;
