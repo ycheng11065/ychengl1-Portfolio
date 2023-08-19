c435b1c2397526d5da48a10bbba0506d7f1eae48
Yu Cheng Li <ychengli11065@gmail.com>
2023-08-18 23:47:34 -0700

    finished creating the dynamic side of the project page

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -33,8 +33,13 @@ const Projects = () => {
     {
       name: 'Subletter',
       category: 'Virtual Marketplace',
-      description:
-        'An easy to use marketplace for university students to find sublets',
+      description: `Amidst the ever-changing landscape of technology, my
+                  fascination for innovation continues to thrive. From crafting
+                  elegant lines of code to diving deep into complex algorithms,
+                  I find solace in the art of problem-solving. Each challenge
+                  presents an opportunity to create, learn, and grow. With an
+                  unwavering curiosity, I embrace the digital realm, driven to
+                  shape the future one algorithm at a time.`,
       year: 2023,
       image: tower_img,
       tech: [
@@ -50,8 +55,13 @@ const Projects = () => {
     {
       name: 'Resume Builder',
       category: 'Web Tool',
-      description:
-        'An intuitive web tool that allows users to easily create a beautiful resume',
+      description: `Amidst the ever-changing landscape of technology, my
+      fascination for innovation continues to thrive. From crafting
+      elegant lines of code to diving deep into complex algorithms,
+      I find solace in the art of problem-solving. Each challenge
+      presents an opportunity to create, learn, and grow. With an
+      unwavering curiosity, I embrace the digital realm, driven to
+      shape the future one algorithm at a time.`,
       year: 2023,
       image: desert_img,
       tech: [
@@ -68,174 +78,109 @@ const Projects = () => {
 
   return (
     <>
-      {projectArray &&
-        projectArray.map((p) => (
-          <Card
-            key={p.id}
-            maw="1000px"
-            h="285px"
-            style={{ border: '1px solid grey' }}
-          >
-            <Flex h="100%" justify="space-between">
-              <Box w="90%">
-                <Flex justify="space-between" pr="20px">
-                  <Text
-                    fw="bold"
-                    style={{ paddingBottom: '0', fontSize: '2rem' }}
-                  >
-                    {p.name}
-                  </Text>
-                  <Group mt="md" mb="xs">
-                    <Badge color="blue" variant="outline">
-                      {p.category}
-                    </Badge>
-                  </Group>
-                </Flex>
-                <Flex direction="column">
-                  <Text mt="5px">{p.description}</Text>
-                  <Flex mt="55px" gap="sm">
-                    {p.tech &&
-                      p.tech.map((t, index) => (
-                        <Badge key={index} color="pink" variant="light">
-                          {t}
-                        </Badge>
-                      ))}
-                  </Flex>
-                </Flex>
-              </Box>
-              <Box p="0" w="500px" h="250px">
-                <BackgroundImage
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                  }}
-                  radius="md"
-                  src={desert_img}
-                />
-              </Box>
-            </Flex>
-          </Card>
-        ))}
-
       <Flex gap="md" direction="column" align="center" my="40px">
-        <Card maw="1000px" h="285px" style={{ border: '1px solid grey' }}>
-          <Flex h="100%" justify="space-between">
-            <Box w="90%">
-              <Flex justify="space-between" pr="20px">
-                <Text
-                  fw="bold"
-                  style={{ paddingBottom: '0', fontSize: '2rem' }}
-                >
-                  NaviBot
-                </Text>
-                <Group mt="md" mb="xs">
-                  <Badge color="blue" variant="outline">
-                    Chatbot
-                  </Badge>
-                </Group>
-              </Flex>
-              <Flex direction="column">
-                <Text mt="5px">
-                  "Amidst the ever-changing landscape of technology, my
-                  fascination for innovation continues to thrive. From crafting
-                  elegant lines of code to diving deep into complex algorithms,
-                  I find solace in the art of problem-solving. Each challenge
-                  presents an opportunity to create, learn, and grow. With an
-                  unwavering curiosity, I embrace the digital realm, driven to
-                  shape the future one algorithm at a time."
-                </Text>
-                <Flex mt="55px" gap="sm">
-                  <Badge color="pink" variant="light">
-                    JavaScript
-                  </Badge>
-                  <Badge color="orange" variant="light">
-                    MySQL
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    NodeJS
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    Express
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    ReactJS
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    CharkraUI
-                  </Badge>
+        {projectArray &&
+          projectArray.map((p, index) => (
+            <Card
+              key={p.id}
+              maw="1000px"
+              h="285px"
+              style={{ border: '1px solid grey' }}
+            >
+              {index % 2 === 0 ? (
+                <Flex h="100%" justify="space-between">
+                  <Box w="90%">
+                    <Flex justify="space-between" pr="20px">
+                      <Text
+                        fw="bold"
+                        style={{ paddingBottom: '0', fontSize: '2rem' }}
+                      >
+                        {p.name}
+                      </Text>
+                      <Group mt="md" mb="xs">
+                        <Badge color="blue" variant="outline">
+                          {p.category}
+                        </Badge>
+                      </Group>
+                    </Flex>
+                    <Flex
+                      h="200px"
+                      direction="column"
+                      style={{ position: 'relative' }}
+                    >
+                      <Text mt="5px">{p.description}</Text>
+                      <Flex
+                        mt="55px"
+                        gap="sm"
+                        style={{ position: 'absolute', bottom: '0' }}
+                      >
+                        {p.tech &&
+                          p.tech.map((t) => (
+                            <Badge key={t.id} color="pink" variant="light">
+                              {t.name}
+                            </Badge>
+                          ))}
+                      </Flex>
+                    </Flex>
+                  </Box>
+                  <Box p="0" w="500px" h="250px">
+                    <BackgroundImage
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                      }}
+                      radius="md"
+                      src={desert_img}
+                    />
+                  </Box>
                 </Flex>
-              </Flex>
-            </Box>
-            <Box p="0" w="500px" h="250px">
-              <BackgroundImage
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                radius="md"
-                src={desert_img}
-              />
-            </Box>
-          </Flex>
-        </Card>
-
-        <Card maw="1000px" h="285px" style={{ border: '1px solid grey' }}>
-          <Flex h="100%" justify="space-between" mr="0">
-            <Box p="0" w="500px" h="250px">
-              <BackgroundImage
-                style={{
-                  width: '100%',
-                  height: '100%',
-                }}
-                radius="md"
-                src={tower_img}
-              />
-            </Box>
-            <Box w="90%" pl="20px">
-              <Flex justify="space-between" pr="20px">
-                <Text fw="bold" style={{ fontSize: '2rem' }}>
-                  Resume Builder
-                </Text>
-                <Group mt="md" mb="xs">
-                  <Badge color="blue" variant="outline">
-                    Chatbot
-                  </Badge>
-                </Group>
-              </Flex>
-              <Flex direction="column">
-                <Text mt="5px">
-                  "Amidst the ever-changing landscape of technology, my
-                  fascination for innovation continues to thrive. From crafting
-                  elegant lines of code to diving deep into complex algorithms,
-                  I find solace in the art of problem-solving. Each challenge
-                  presents an opportunity to create, learn, and grow. With an
-                  unwavering curiosity, I embrace the digital realm, driven to
-                  shape the future one algorithm at a time."
-                </Text>
-                <Flex mt="55px" gap="sm">
-                  <Badge color="pink" variant="light">
-                    JavaScript
-                  </Badge>
-                  <Badge color="orange" variant="light">
-                    MySQL
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    NodeJS
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    Express
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    ReactJS
-                  </Badge>
-                  <Badge color="green" variant="light">
-                    CharkraUI
-                  </Badge>
+              ) : (
+                <Flex h="100%" justify="space-between" mr="0">
+                  <Box p="0" w="500px" h="250px">
+                    <BackgroundImage
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                      }}
+                      radius="md"
+                      src={tower_img}
+                    />
+                  </Box>
+                  <Box w="90%" pl="20px">
+                    <Flex justify="space-between" pr="20px">
+                      <Text fw="bold" style={{ fontSize: '2rem' }}>
+                        {p.name}
+                      </Text>
+                      <Group mt="md" mb="xs">
+                        <Badge color="blue" variant="outline">
+                          {p.category}
+                        </Badge>
+                      </Group>
+                    </Flex>
+                    <Flex
+                      h="200px"
+                      direction="column"
+                      style={{ position: 'relative' }}
+                    >
+                      <Text mt="5px">{p.description}</Text>
+                      <Flex
+                        mt="55px"
+                        gap="sm"
+                        style={{ position: 'absolute', bottom: '0' }}
+                      >
+                        {p.tech &&
+                          p.tech.map((t) => (
+                            <Badge key={t.id} color="pink" variant="light">
+                              {t.name}
+                            </Badge>
+                          ))}
+                      </Flex>
+                    </Flex>
+                  </Box>
                 </Flex>
-              </Flex>
-            </Box>
-          </Flex>
-        </Card>
+              )}
+            </Card>
+          ))}
       </Flex>
     </>
   );
