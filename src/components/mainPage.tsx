7fdf70ab1cc61bcb983d3ab4eeccf3032167c3c4
Yu Cheng Li <ychengli11065@gmail.com>
2023-08-19 16:20:07 -0700

    made the navigation buttons look better and the resume button is able to redirect to a resume page

--- src/components/mainPage.tsx
+++ src/components/mainPage.tsx
@@ -4,17 +4,25 @@ const MainPage = () => {
   return (
     <>
       <Flex mt="20px" align="center" direction="column">
-        <Text ta="center" style={{ fontSize: "5rem" }}> Hi There... I'm YuCheng Li</Text>
-        <Text ta="center" style={{ maxWidth: "600px"}}>
-          I am a vancouver based software engineer. Having grown up between the vibrant cities of Montreal and Calgary, 
-          I am now a senior computer science student at the University of British Columbia.
-          
-          When I'm not buried in assignments, you'll find me exploring the realm of software and languages. There's nothing more furfilling
-          to me than translating newfound skills into innovative and practical projects.
-
-          Off-screen, you'll find me enjoying a game of badminton, delving into the intricacies of the Japanese language, or humbling myself
-          in the world of Summoner's rift.
-
+        <Text ta="center" style={{ fontSize: '5rem' }}>
+          Hi There... I'm YuCheng
+        </Text>
+        <Text style={{ maxWidth: '600px' }}>
+          I am a Vancouver based software engineer. Having grown up between the
+          vibrant cities of Montreal and Calgary, I am now a senior computer
+          science student at the University of British Columbia.
+        </Text>
+        <Text style={{ maxWidth: '600px' }}>
+          When I'm not buried in assignments, you'll find me exploring the realm
+          of software and languages. There's nothing more furfilling to me than
+          translating newfound skills into innovative and practical projects.
+        </Text>
+        <Text style={{ maxWidth: '600px' }}>
+          Off-screen, you'll find me enjoying a game of badminton, delving into
+          the intricacies of the Japanese language, or humbling myself in the
+          world of Summoner's Rift.
+        </Text>
+        <Text ta="right" style={{ maxWidth: '600px' }}>
           Anyways, I hope you enjoy my corner of the web
         </Text>
       </Flex>
--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,35 +1,101 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Text,
   Flex,
   Container,
   Button,
   useMantineColorScheme,
   ActionIcon,
+  createStyles,
 } from '@mantine/core';
 import { IconSun, IconMoon } from '@tabler/icons-react';
+import { Document, Page } from 'react-pdf';
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+// import resume from '../resume/ychengli_2023winter_resume.pdf';
+
+const useStyles = createStyles((theme) => ({
+  linkButton: {
+    textTransform: 'uppercase',
+    fontSize: '15px',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+    padding: '5px',
+    borderBottom: `4px solid transparent`,
+    transition: 'border-color 100ms ease, color 100ms ease',
+    textDecoration: 'none',
+
+    '&:hover': {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      textDecoration: 'none',
+    },
+  },
+  linkActive: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    borderBottomColor:
+      theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 6],
+  },
+}));
 
 const NavBar = () => {
+  const { classes } = useStyles();
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const [activeLinkIndex, setActiveLink] = useState<number>(0);
   const dark = colorScheme === 'dark';
 
+  const openPDF = () => {
+    const pdfPath = require('../resume/ychengli_2023winter_resume.pdf'); 
+    fetch(pdfPath)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const pdfUrl = URL.createObjectURL(blob);
+        window.open(pdfUrl, '_blank');
+      });
+  };
   return (
     <>
       <Flex mt="10px" direction="row" justify="space-between">
         <Container ml="10px" mr="0">
-          <Link to="./" style={{ textDecoration: 'none', color: 'inherit' }}>
-            <Text style={{ fontSize: '2rem' }}>YCL</Text>
+          <Link
+            to="./"
+            style={{
+              textDecoration: 'none',
+              color: 'inherit',
+              fontSize: '2rem',
+            }}
+            onClick={(event) => {
+              setActiveLink(1);
+            }}
+          >
+            YCL
           </Link>
         </Container>
         <Container mx="0" mt="15px">
           <Flex gap="md">
-            <Link to="/projects">
-              <Button>Projects</Button>
+            <Link
+              to="/projects"
+              className={`${classes.linkButton} ${
+                activeLinkIndex === 0 ? classes.linkActive : ''
+              }`}
+              onClick={(event) => {
+                setActiveLink(0);
+              }}
+            >
+              Project
             </Link>
-            <Link to="/">
-              <Button>About</Button>
+            <Link
+              to="/"
+              className={`${classes.linkButton} ${
+                activeLinkIndex === 1 ? classes.linkActive : ''
+              }`}
+              onClick={(event) => {
+                setActiveLink(1);
+              }}
+            >
+              About
             </Link>
-            <Button>Resume</Button>
+            <Button onClick={openPDF}>Resume</Button>
             <ActionIcon
               size="2.2rem"
               variant="outline"
