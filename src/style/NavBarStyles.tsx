cbc02f6686352a9bf5b7e014f623604226f58c20
Yu Cheng Li <ychengli11065@gmail.com>
2023-08-30 17:49:05 -0700

    added gif

---
+++ src/style/NavBarStyles.tsx
@@ -0,0 +1,67 @@
+import { createStyles } from '@mantine/core';
+
+const useStyles = createStyles((theme) => ({
+  navBar: {
+    fontColor: 'white',
+    backgroundColor: theme.colorScheme === 'dark' ? 'black' : 'white',
+  },
+  linkButton: {
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    fontSize: '15px',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+    padding: '5px',
+    borderBottom: `2px solid transparent`,
+    transition: 'border-color 100ms ease, color 100ms ease',
+    textDecoration: 'none',
+
+    '&:hover': {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      textDecoration: 'none',
+    },
+  },
+  linkButtonMobile: {
+    fontSize: '2rem',
+    cursor: 'pointer',
+    textTransform: 'uppercase',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+    padding: '5px',
+    borderBottom: `2px solid transparent`,
+    transition: 'border-color 100ms ease, color 100ms ease',
+    textDecoration: 'none',
+
+    '&:hover': {
+      color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+      textDecoration: 'none',
+    },
+  },
+  linkActive: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    borderBottomColor:
+      theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 5 : 6],
+  },
+  colorModeButton: {
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[1]
+        : theme.colors.gray[6],
+  },
+  logoButton: {
+    textDecoration: 'none',
+    color: theme.colorScheme === 'dark' ? 'white' : 'black',
+    fontSize: '2rem',
+  },
+  optionIcon: {
+    fontSize: '2rem',
+    margin: 'auto 0',
+    color: 'white',
+  },
+}));
+
+export { useStyles };
