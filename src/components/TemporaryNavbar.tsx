0a3875bc416abc89d7e03abde9c899b2864927e7
ycheng11065 <ychengli11065@gmail.com>
2024-10-24 01:44:41 -0700

    removed more unused code

--- src/components/TemporaryNavbar.tsx
+++ src/components/TemporaryNavbar.tsx
@@ -1,5 +1,4 @@
 // @ts-nocheck
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Link as ScrollLink } from "react-scroll";
 import { useDisclosure } from "@mantine/hooks";
